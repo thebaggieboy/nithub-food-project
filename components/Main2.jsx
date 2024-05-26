4965a0b6310ccda927a12fe842227b4ed249512f
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-05-26 11:39:36 +0100

    guietta

--- components/Main2.jsx
+++ components/Main2.jsx
@@ -47,14 +47,14 @@ console.log(average_month_price.percentage_change)
      
   <ul class="flex flex-wrap space-x-2 pt-4  mt-2 ml-10 pl-10 text-sm font-medium text-center text-gray-500 dark:text-gray-400">
       <li class="me-2 mt-2">
-      <span class="bg-green-800 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"></span> Current Year
+      <span class="bg-green-800 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"></span> <span class="text-xs" style={{fontSize:10}}> Current Year</span>
       </li>
       <li class="me-2 mt-2">
-      <span class="bg-green-400 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"></span> Previous Year
+      <span class="bg-green-400 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"></span> <span class="text-xs" style={{fontSize:10}}> Previous Year</span>
       </li>
       <li class="me-2">
-      <div className="p-10 ml-5">
-<Dropdown className="" dismissOnClick={true} label="2024" style={{backgroundColor:"white",textAlign:"center", color:"black", border:"1px solid green", fontSize:8, maxWidth:"fit-content", marginInline:"auto" }}>
+      <div className="p-10 ml-5" style={{fontSize:8}}>
+<Dropdown className="text-xs" dismissOnClick={true} label="2024" style={{backgroundColor:"white",textAlign:"center", color:"black", border:"0.5px solid green",  maxWidth:"fit-content", marginInline:"auto" }}>
       <Dropdown.Item as={Link} href="?dashboard=Daily">
         2023
       </Dropdown.Item>
